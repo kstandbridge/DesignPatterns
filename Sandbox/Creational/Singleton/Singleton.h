3d98e37f454679ff46817a4af878c5b6965c8398
Kenneth Standbridge <kstandbridge@blizzard.com>
2017-12-21 17:21:53 +0000

    fix: gtest

--- Sandbox/Creational/Singleton/Singleton.h
+++ Sandbox/Creational/Singleton/Singleton.h
@@ -6,50 +6,52 @@
 #include <boost/lexical_cast.hpp>
 #include <vector>
 
-class Database
+namespace Singleton
 {
-public:
-  virtual int get_population(const std::string& name) = 0;
-};
+	class Database
+	{
+	public:
+		virtual int get_population(const std::string& name) = 0;
+	};
 
-class SingletonDatabase : public Database
-{
-  SingletonDatabase()
-  {
-    std::cout << "Initializing database" << std::endl;
+	class SingletonDatabase : public Database
+	{
+		SingletonDatabase()
+		{
+			std::cout << "Initializing database" << std::endl;
 
-    std::ifstream ifs("capitals.txt");
+			std::ifstream ifs("capitals.txt");
 
-    std::string s, s2;
-    while (getline(ifs, s))
-    {
-      getline(ifs, s2);
-      int pop = boost::lexical_cast<int>(s2);
-      capitals[s] = pop;
-    }
-    instance_count++;
-  }
+			std::string s, s2;
+			while (getline(ifs, s))
+			{
+				getline(ifs, s2);
+				int pop = boost::lexical_cast<int>(s2);
+				capitals[s] = pop;
+			}
+			instance_count++;
+		}
 
-  std::map<std::string, int> capitals;
+		std::map<std::string, int> capitals;
 
-public:
-  static int instance_count;
+	public:
+		static int instance_count;
 
-  SingletonDatabase(SingletonDatabase const&) = delete;
-  void operator=(SingletonDatabase const&) = delete;
+		SingletonDatabase(SingletonDatabase const&) = delete;
+		void operator=(SingletonDatabase const&) = delete;
 
-  static SingletonDatabase& get()
-  {
-    static SingletonDatabase db;
-    return db;
-  }
+		static SingletonDatabase& get()
+		{
+			static SingletonDatabase db;
+			return db;
+		}
 
-  int get_population(const std::string& name) override
-  {
-    return capitals[name];
-  }
+		int get_population(const std::string& name) override
+		{
+			return capitals[name];
+		}
 
-  /*
+		/*
   static SingletonDatabase* get_instance()
   {
     if (!instance)
@@ -57,53 +59,56 @@ public:
     return instance; // atexit
   }
   */
-};
+	};
 
-//int SingletonDatabase::instance_count = 0;
+	//int SingletonDatabase::instance_count = 0;
 
-class DummyDatabase : public Database
-{
-  std::map<std::string, int> capitals;
-public:
+	class DummyDatabase : public Database
+	{
+		std::map<std::string, int> capitals;
+	public:
 
 
-  DummyDatabase()
-  {
-    capitals["alpha"] = 1;
-    capitals["beta"] = 2;
-    capitals["gamma"] = 3;
-  }
+		DummyDatabase()
+		{
+			capitals["alpha"] = 1;
+			capitals["beta"] = 2;
+			capitals["gamma"] = 3;
+		}
 
-  int get_population(const std::string& name) override {
-    return capitals[name];
-  }
-};
+		int get_population(const std::string& name) override {
+			return capitals[name];
+		}
+	};
 
-struct SingletonRecordFinder
-{
-  int total_population(std::vector<std::string> names)
-  {
-    int result = 0;
-    for (auto& name : names)
-      result += SingletonDatabase::get().get_population(name);
-    return result;
-  }
-};
+	struct SingletonRecordFinder
+	{
+		int total_population(std::vector<std::string> names)
+		{
+			int result = 0;
+			for (auto& name : names)
+				result += SingletonDatabase::get().get_population(name);
+			return result;
+		}
+	};
 
-struct ConfigurableRecordFinder
-{
-  explicit ConfigurableRecordFinder(Database& db)
-    : db{db}
-  {
-  }
+	struct ConfigurableRecordFinder
+	{
+		explicit ConfigurableRecordFinder(Database& db)
+			: db{db}
+		{
+		}
 
-  int total_population(std::vector<std::string> names)
-  {
-    int result = 0;
-    for (auto& name : names)
-      result += db.get_population(name);
-    return result;
-  }
+		int total_population(std::vector<std::string> names)
+		{
+			int result = 0;
+			for (auto& name : names)
+				result += db.get_population(name);
+			return result;
+		}
 
-  Database& db;
-};
+		Database& db;
+	};
+
+	int SingletonDatabase::instance_count = 0;
+}
