98e2457509d18dce8cadf48e06a3f7abfc9b0fc4
Kenneth Standbridge <kstandbridge@blizzard.com>
2018-01-19 11:33:35 +0000

    Facade

---
+++ DesignPatterns/Facade.cpp
@@ -0,0 +1,10 @@
+namespace Facade
+{
+	// Provides a simple, wasy to understand/use interface over a large and sophisticated body of code
+	// Make a library easier to understand, use and test
+	// Reduce dependencies of user code on internal APIs that may change
+	// Allow more flexibility in developing/refactoring the library
+	// Wrap a poorly designed collection of APIs with a single well-designed API
+
+	//TODO Implement example of Facade
+}
